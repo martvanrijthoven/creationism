5b506e095da8402dc2eb1766c6b01f07ff8b8ebf
martvanrijthoven <mart.vanrijthoven@gmail.com>
2022-04-05 13:31:09 +0200

    v0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='creationism',
-    version='0.0.4',
+    version='0.0.5',
     author='Mart van Rijthoven',
     author_email='mart.vanrijthoven@gmail.com',
     packages=find_packages(exclude=("tests",'notebooks')),
